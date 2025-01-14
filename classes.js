3e4f31cfd2c7c73be71f35bce6f91b51476091d4
Offixialtrust <richardtrust12@gmail.com>
2025-01-14 15:09:57 +0100

    update1

---
+++ classes.js
@@ -0,0 +1,25 @@
+class Account {
+    constructor(name, bal) {
+        this.name = name;
+        this.balance = bal;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
+
+    withdraw(amount) {
+        if (amount > this.balance) {
+            return "insufficient funds";
+        } else {
+            this.balance -= amount;
+        }
+    }
+}
+
+const acc1 = new Account("Trust Richard", 5000);
+
+acc1.deposit(2000);
+acc1.withdraw(800);
+
+console.log(acc1.balance);
